5285b7971844c983cb34547897af07fbea154912
Brian McConeghy <brianmcconeghy@gmail.com>
2025-09-20 20:27:49 -0700

    Make client using a function to call within other helpers

--- polygon/fetch.py
+++ polygon/fetch.py
@@ -1,24 +1,27 @@
 import enum
 import os
-from dateutil.parser import parse as dateutil_parser
 from pathlib import Path
 
 import boto3
 from botocore.config import Config
+from dateutil.parser import parse as dateutil_parser
 
 BUCKET_NAME = "flatfiles"
 ROOT_DIR = Path(os.environ.get("ROOT_DIR"))
 
-session = boto3.Session(
-    aws_access_key_id=os.environ.get("POLYGON_API_KEY_ID"),
-    aws_secret_access_key=os.environ.get("POLYGON_API_SECRET_ACCESS_KEY"),
-)
 
-s3 = session.client(
-    "s3",
-    endpoint_url="https://files.polygon.io",
-    config=Config(signature_version="s3v4"),
-)
+def get_s3_client():
+    """Create an S3 client for Polygon's S3-compatible storage."""
+    session = boto3.Session(
+        aws_access_key_id=os.environ.get("POLYGON_API_KEY_ID"),
+        aws_secret_access_key=os.environ.get("POLYGON_API_SECRET_ACCESS_KEY"),
+    )
+    s3_client = session.client(
+        "s3",
+        endpoint_url="https://files.polygon.io",
+        config=Config(signature_version="s3v4"),
+    )
+    return s3_client
 
 
 class PolygonDataSourceBasePrefix(enum.StrEnum):
@@ -45,9 +48,12 @@ def get_newest_flat_files_for_prefix(
     level: PolygonDataSourceLevel = PolygonDataSourceLevel.minute_aggs_v1,
     bucket_name: str = BUCKET_NAME,
     num_files: int = 1,
+    s3_client=None,
 ) -> list[str]:
     """Get the newest file for a given combination of base prefix and level."""
-    paginator = s3.get_paginator("list_objects_v2")
+    if s3_client is None:
+        s3_client = get_s3_client()
+    paginator = s3_client.get_paginator("list_objects_v2")
 
     all_objects = [
         obj["Key"]
@@ -67,8 +73,11 @@ def download_file_from_s3(
     object_key: str,
     bucket_name: str = BUCKET_NAME,
     root_dir: str = ROOT_DIR,
+    s3_client=None,
 ) -> None:
     """Download a file from S3 to a local directory."""
+    if s3_client is None:
+        s3_client = get_s3_client()
     local_file_name = object_key.split("/")[-1]
     local_file_path = root_dir / local_file_name
-    s3.download_file(bucket_name, object_key, local_file_path)
+    s3_client.download_file(bucket_name, object_key, local_file_path)
